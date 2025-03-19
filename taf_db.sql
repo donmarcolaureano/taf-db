a3abcd5b256731b5ee11e87fe625388ab270c6ee
Marco Laureano <laureano.marco@gmail.com>
2025-03-18 19:18:58 -0700

    Update taf_db.sql

--- taf_db.sql
+++ taf_db.sql
@@ -1,2 +1,17 @@
-show tables;
-select * from employees
+DROP SCHEMA IF EXISTS taf_db;
+CREATE SCHEMA IF NOT EXISTS taf_db DEFAULT CHARACTER SET utf8;
+USE taf_db;
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(20) NULL,
+    hire_date DATE NOT NULL,
+    job_title VARCHAR(50) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department VARCHAR(50) NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+) ENGINE = InnoDB;
